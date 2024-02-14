9b5f767dfd5f10abab083ecd571685deb2cd60a5
syedamir5560 <syedamir5560@gmail.com>
2024-02-15 02:53:55 +0530

    remove addd classs

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const modal = document.querySelectorAll('.services__model'),
+ modalButton = document.querySelectorAll('.services__button'),
+ modalClose = document.querySelectorAll('.services__model-close')
+
+ let activeModal = (modalClick) =>{
+    modal[modalClick].classList.add('active-modal')
+ }
+
+ modalButton.forEach((modalButton,i)=>{
+    modalButton.addEventListener('click',()=>{
+            activeModal(i)
+    })
+ })
+
+ modalClose.forEach((modalClose)=>{
+    modalClose.addEventListener('click',()=>{
+        modal.forEach((modalRemove)=>{
+            modalRemove.classList.remove('active-modal')
+        })
+
+    })
+ })
