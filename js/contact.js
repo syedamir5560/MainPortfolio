e25f24628776a67c966feb382d32fe98ada40030
syedamir5560 <syedamir5560@gmail.com>
2024-02-17 00:28:06 +0530

    ss

---
+++ js/contact.js
@@ -0,0 +1,31 @@
+const contactForm  = document.getElementById('contact-form')
+const contactMessage = document.getElementById('contact-message')
+
+const sendEmail=(e)=>{
+    e.preventDefault() 
+
+    // serviceid - templateid - #form - public key
+
+        emailjs.sendForm('service_qd3cpr3','template_i4b82bn','#contact-form','CJDcgwuMh3dp3zZSO')
+        .then(()=>{
+            // show send message
+            contactMessage.textContent = 'Message Send Successfully!!!'
+            // console.log("submit")
+
+            // remove meassage
+
+            setTimeout(()=>{
+                contactMessage.textContent=''
+            },5000)
+
+            //clear input field
+
+            contactForm.reset()
+
+        },()=>{
+            contactMessage.textContent = "Message not sent service error"
+        })
+ 
+}
+
+contactForm.addEventListener('submit' , sendEmail)
