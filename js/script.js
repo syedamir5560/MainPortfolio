dd82dea50445bbcc962c8644378865c954522985
syedamir5560 <syedamir5560@gmail.com>
2024-03-26 12:02:31 +0530

    gsap

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ gsap.registerPlugin(ScrollTrigger);
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("body"),
+    el: document.querySelector(".main"),
     smooth: true
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "body" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy("body", {
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
     scrollTop(value) {
         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
@@ -18,7 +18,7 @@ ScrollTrigger.scrollerProxy("body", {
         return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("body").style.transform ? "transform" : "fixed"
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
 });
 
 
@@ -87,15 +87,56 @@ gsap.from('.home-container   .home-perfil',{
 })
 
 gsap.from('.services .services__card', {
-    scale: 0.7,
-    duration: 1,
+    scale: 0.2,
+    duration: 0.8,
     opacity: 0,
     scrollTrigger: {
-        trigger: ".services",
-        start: 'top 100%',
-        scroller: 'body',
+        trigger: ".services .services__card",
+        start: 'top 80%',
+        scroller: '.main',
         // markers: true,
-        scrub: true
+        // scrub: true
+    }
+})
+
+
+gsap.from('.work__container .work__card', {
+    scale: 0.2,
+    duration: 0.8,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".work__container .work__card",
+        start: 'top 80%',
+        scroller: '.main',
+        // markers: true,
+        // scrub: true
+    }
+})
+
+gsap.from('.contact .contact__container', {
+    scale: 0.2,
+    duration: 0.8,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".contact .contact__container",
+        start: 'top 80%',
+        scroller: '.main',
+        // markers: true,
+        // scrub: true
+    }
+})
+
+
+gsap.from('.footer .footer__container', {
+    scale: 0.2,
+    duration: 0.8,
+    opacity: 0,
+    scrollTrigger: {
+        trigger: ".footer .footer__container",
+        start: 'top 100%',
+        scroller: '.main',
+        // markers: true,
+        // scrub: true
     }
 })
 
